d52ff39b5baa8d8fab3e2dbde45b15e38e7cad18
shy8818 <83875932+shyhwan@users.noreply.github.com>
2023-06-24 00:19:58 +0900

    nodemon 적용

---
+++ app/app.js
@@ -0,0 +1,17 @@
+"use strict"; // ECMA Script를 준수하기 위해 작성한다.
+
+// 모듈
+const express = require("express");
+const app = express();
+
+// 라우팅
+const home = require("./src/routes/home");
+
+//앱 세팅
+app.set("views", "./src/views"); // 뷰의 위치를 지정한다.
+app.set("view engine", "ejs");
+
+app.use(express.static(`${__dirname}/src/public`));
+app.use("/", home); // use는 미들 웨어를 등록해주는 메서드이다.
+
+module.exports = app;
---
+++ app/bin/www.js
@@ -0,0 +1,9 @@
+"use strict"; // ECMA Script를 준수하기 위해 작성한다.
+
+const app = require("../app");
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log("서버 시작!");
+});
