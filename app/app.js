1f0f40809aa6dceaf54e83f9092810c2d3cbbf87
shy8818 <83875932+shyhwan@users.noreply.github.com>
2023-06-24 01:38:59 +0900

    부트스트랩 적용

--- app/app.js
+++ app/app.js
@@ -12,6 +12,7 @@ app.set("views", "./src/views"); // 뷰의 위치를 지정한다.
 app.set("view engine", "ejs");
 
 app.use(express.static(`${__dirname}/src/public`));
+app.use("/css", express.static(`${__dirname}/node_modules/bootstrap/dist/css`));
 app.use("/", home); // use는 미들 웨어를 등록해주는 메서드이다.
 
 module.exports = app;
